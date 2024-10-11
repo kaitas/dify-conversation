75a684a3fa87d692a2d2d13a9bd4531f07ca3283
Akihiko SHIRAI, Ph.D <shirai@mail.com>
2024-10-11 20:24:05 +0900

    title

--- config/index.ts
+++ config/index.ts
@@ -3,11 +3,11 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
-  privacy_policy: '',
-  default_language: 'en',
+  title: 'ガンマミィ ジェネレーター',
+  description: 'Generate Cute Gammamy pictures!',
+  copyright: 'BlendAI and AICU Inc.',
+  privacy_policy: 'https://corp.aicu.ai/ja/privacy',
+  default_language: 'ja',
 }
 
 export const isShowPrompt = false
