2f2184b6eb5cc6e280ccd4565be3ae3255e6bbe6
Akihiko SHIRAI, Ph.D <shirai@mail.com>
2024-10-11 21:16:44 +0900

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,7 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'ガンマミィ ジェネレーター',
+  title: 'ガンマミィ・ジェネレーター',
   description: '例：「踊っているガンマミィ画像を生成して」「photo realistic 3d illustration of gammamy, dancing...」',
   copyright: 'BlendAI and AICU Inc.',
   privacy_policy: 'https://corp.aicu.ai/ja/privacy',
@@ -11,7 +11,7 @@ export const APP_INFO: AppInfo = {
 }
 
 export const isShowPrompt = false
-export const promptTemplate = 'I want you to act as a javascript console.'
+export const promptTemplate = 'ガンマミィの可愛いイラストを生成して'
 
 export const API_PREFIX = '/api'
 
